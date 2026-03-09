e4d2ad91951baa20f8d4f82210a99b0a33b9f6a1
fantasystar94 <aniseed_3@naver.com>
2026-03-09 23:00:36 +0900

    ✨ feat: DB, yml, security 초기 세팅 - yml 이 env 를 바라보게 수정 - application.java 에 EnableJpaAuditing 추가 - BaseEntity 생성 - build.gradle 의존성 수정 - SecurityConfig mvp 패턴에 따라 모든 접근 허용 - test.http 생성 - user 엔티티 생성 - 유저 컨트롤러, 서비스 최소기능 구현 및 작동확인

--- src/main/java/com/example/backendquiz/BackendQuizApplication.java
+++ src/main/java/com/example/backendquiz/BackendQuizApplication.java
@@ -2,8 +2,10 @@ package com.example.backendquiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendQuizApplication {
 
     public static void main(String[] args) {
